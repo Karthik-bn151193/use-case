7811ac6ccdb02328615c13b53515c5595cd0d105
Karthik-bn151193 <karthik.bn15111993@gmail.com>
2025-04-17 10:28:16 +0530

    Update main.tf

--- albpath/main.tf
+++ albpath/main.tf
@@ -16,7 +16,7 @@ module "ec2" {
   ami_id = "ami-0f9de6e2d2f067fca"
   instance_type = "t2.micro"
   subnet_ids = module.vpc.subnet_ids
-  key_name = "hcl"
+  key_name = "useast"
   vpc_id = module.vpc.vpc_id
   target_group_a_arn = module.alb.target_group_a_arn
   target_group_b_arn = module.alb.target_group_b_arn
